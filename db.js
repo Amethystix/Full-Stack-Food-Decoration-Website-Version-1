5a937ce3c92b49efbba952da9be51584de608f87
lad475 <lad475@nyu.edu>
2017-11-09 20:56:30 -0500

    bootstrap done

--- db.js
+++ db.js
@@ -3,21 +3,26 @@
 var mongoose = require('mongoose');
 //my schema goes here!
 const Accessory = new mongoose.Schema({
-  xPos: //number representing x coordinate of the accessory on the fruit image
-  yPos: //number representing y coordinate of the accessory on the fruit image
+  xPos: Number, //number representing x coordinate of the accessory on the fruit image
+  yPos: Number, //number representing y coordinate of the accessory on the fruit image
 
-  name: //string representing the name of the accessory
+  name: String, //string representing the name of the accessory
 
-  xSize: //number representing the width of the accessory
-  ySize: //number representing the height of the accessory
+  xSize: Number, //number representing the width of the accessory
+  ySize: Number //number representing the height of the accessory
 });
 const Fruit = new mongoose.Schema({
-  user: //reference to a user object who created the fruit
-  fruitType: //type of fruit in the user's image
-  accessories: //list of accessory objects
+  user: String, //reference to a user object who created the fruit
+  fruitType: String, //type of fruit in the user's image
+  accessories: [Accessory] //list of accessory objects
 });
-
+const Testimonial = new mongoose.Schema({
+	name: String, //Name representing the person who posted the testimonial
+	contents: String,
+	date: Date //Date posted
+});
 mongoose.model('Accessory', Accessory);
 mongoose.model('Fruit', Fruit);
+mongoose.model('Testimonial', Testimonial)
 
 mongoose.connect('mongodb://localhost/final');
